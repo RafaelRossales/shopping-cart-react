e83c51a1b2b486b8616d7e0c09706c97ea3cffa2
RafaelRossales <rapha2704@gmail.com>
2021-04-05 21:49:36 -0300

    Creating cart hooks

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -5,8 +5,8 @@ import {
   MdRemoveCircleOutline,
 } from 'react-icons/md';
 
-// import { useCart } from '../../hooks/useCart';
-// import { formatPrice } from '../../util/format';
+import { useCart } from '../../hooks/useCart';
+import { formatPrice } from '../../util/format';
 import { Container, ProductTable, Total } from './styles';
 
 interface Product {
@@ -18,28 +18,38 @@ interface Product {
 }
 
 const Cart = (): JSX.Element => {
-  // const { cart, removeProduct, updateProductAmount } = useCart();
 
-  // const cartFormatted = cart.map(product => ({
-  //   // TODO
-  // }))
-  // const total =
-  //   formatPrice(
-  //     cart.reduce((sumTotal, product) => {
-  //       // TODO
-  //     }, 0)
-  //   )
+  const { cart, removeProduct, updateProductAmount } = useCart();
+
+  const cartFormatted = cart.map(product => ({
+    // TODO
+    ...product,
+    priceFormatted: formatPrice(product.price),
+    subTotal: formatPrice(product.price * product.amount)
+  }))
+
+
+  const total =
+    formatPrice(
+      cart.reduce((sumTotal, product) => {
+        return sumTotal + product.price * product.amount
+      }, 0)
+    )
 
   function handleProductIncrement(product: Product) {
     // TODO
+    updateProductAmount({productId:product.id,amount:product.amount + 1});
   }
 
   function handleProductDecrement(product: Product) {
     // TODO
+
+    updateProductAmount({productId:product.id,amount:product.amount - 1});
   }
 
   function handleRemoveProduct(productId: number) {
     // TODO
+    removeProduct(productId);
   }
 
   return (
@@ -55,61 +65,63 @@ const Cart = (): JSX.Element => {
           </tr>
         </thead>
         <tbody>
-          <tr data-testid="product">
+          {cartFormatted.map(_product =>(
+            <tr key={_product.id} data-testid="product">
             <td>
-              <img src="https://rocketseat-cdn.s3-sa-east-1.amazonaws.com/modulo-redux/tenis1.jpg" alt="Tênis de Caminhada Leve Confortável" />
+              <img src={_product.image} alt={_product.title} />
             </td>
             <td>
-              <strong>Tênis de Caminhada Leve Confortável</strong>
-              <span>R$ 179,90</span>
+              <strong>{_product.title}</strong>
+              <span>R$ {_product.price}</span>
             </td>
             <td>
               <div>
                 <button
                   type="button"
                   data-testid="decrement-product"
-                // disabled={product.amount <= 1}
-                // onClick={() => handleProductDecrement()}
-                >
+                  disabled={_product.amount <= 1}
+                  onClick={() => handleProductDecrement(_product)}
+                  >
                   <MdRemoveCircleOutline size={20} />
                 </button>
                 <input
                   type="text"
                   data-testid="product-amount"
                   readOnly
-                  value={2}
-                />
+                  value={_product.amount}
+                  />
                 <button
                   type="button"
                   data-testid="increment-product"
-                // onClick={() => handleProductIncrement()}
-                >
+                  onClick={() => handleProductIncrement(_product)}
+                  >
                   <MdAddCircleOutline size={20} />
                 </button>
               </div>
             </td>
             <td>
-              <strong>R$ 359,80</strong>
+              <strong>R$ {_product.price}</strong>
             </td>
             <td>
               <button
                 type="button"
                 data-testid="remove-product"
-              // onClick={() => handleRemoveProduct(product.id)}
-              >
+                onClick={() => handleRemoveProduct(_product.id)}
+                >
                 <MdDelete size={20} />
               </button>
             </td>
           </tr>
-        </tbody>
+      ))}
+      </tbody>
       </ProductTable>
-
       <footer>
+
         <button type="button">Finalizar pedido</button>
 
         <Total>
           <span>TOTAL</span>
-          <strong>R$ 359,80</strong>
+          <strong>R$ {total }</strong>
         </Total>
       </footer>
     </Container>
